3ddaa7d2c478ca779b9a9dc0903ad82be9055a0c
mathcodes <jonpchristie@gmail.com>
2022-12-15 17:44:49 -0500

    creating notes logic complete

--- TS-todo-tags/src/NoteForm.tsx
+++ TS-todo-tags/src/NoteForm.tsx
@@ -1,6 +1,6 @@
 import { FormEvent, useRef, useState } from 'react'
 import { Form, Stack, Row, Col, Button } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import CreatableReactSelect from "react-select/creatable"
 import { NoteData, Tag } from './App'
 import { v4 as uuidV4 } from 'uuid'
@@ -15,6 +15,7 @@ export function NoteForm({ onSubmit, onAddTag, availableTags }: NoteFormProps) {
   const titleRef = useRef<HTMLInputElement>(null)
   const markdownRef = useRef<HTMLTextAreaElement>(null)
   const [selectedTags, setSelectedTags] = useState<Tag[]>([])
+  const navigate = useNavigate()
 
   function handleSubmit(e: FormEvent) {
     e.preventDefault()
@@ -22,8 +23,10 @@ export function NoteForm({ onSubmit, onAddTag, availableTags }: NoteFormProps) {
     onSubmit({
       title: titleRef.current!.value, // '!' means it will never be null as we have required them
       markdown: markdownRef.current!.value,
-      tags: [...selectedTags]
+      tags: selectedTags,
     })
+
+    navigate('..')
   }
   
 
