962a357948c373e42b407cddd2f2132706d84913
mathcodes <jonpchristie@gmail.com>
2022-12-15 02:12:18 -0500

    state

--- TS-todo-tags/src/useLocalStorage.tsx
+++ TS-todo-tags/src/useLocalStorage.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 export function useLocalStorage<T>(key: string, initialValue:T | (()=>T)) {
   const [value, setValue ] = useState<T>(()=>{
@@ -14,5 +14,9 @@ export function useLocalStorage<T>(key: string, initialValue:T | (()=>T)) {
       return JSON.parse(jsonValue)
     }
   })
+useEffect(() => {
+  localStorage.setItem(key, JSON.stringify(value))
+}, [value, key])
 
+  return [value, setValue] as [T, typeof setValue]
 }
